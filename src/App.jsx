0c98784fef6d3a506ac6076fba10c6e3dcaed017
Mridulians <gmridul898@gmail.com>
2024-06-25 16:44:35 +0530

    remaining sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Banner from './components/Banner/Banner';
+import Exp from './components/Experience/Exp';
 import Navbar from './components/Navbar/Navbar'
+import Project from './components/Projects/Project';
+import Skill from './components/Skills/Skill';
 
 function App() {
   return <>
   <Navbar />
   <Banner/>
+  <Skill />
+  <Project />
+  <Exp />
   </>
 }
 
---
+++ src/components/Projects/Project.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+
+const Project = () => {
+  return (
+    <div className="bg-black">
+      <div className="p-[2rem]">
+        <h1 className="text-white text-[3rem] opacity-[0.8] font-bold font-serif text-center">
+          Projects
+        </h1>
+
+        <p className="text-white text-[1.3rem] opacity-[0.5] font-normal font-serif mt-[20px] mb-[2rem] text-center">
+          &quot; Explore a curated selection of my diverse projects, showcasing
+          expertise in web development, data analysis, and innovative
+          problem-solving. Each project highlights my skills in coding, design,
+          and collaboration, demonstrating a commitment to delivering
+          high-quality, impactful solutions in various domains.&quot;
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
